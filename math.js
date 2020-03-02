fa201c56f897ae5b58e923181c8b1e5e6f823e65
kyran lilley <kyran-lilley@live.nmit.ac.nz>
2020-03-03 09:47:51 +1300

    new commit

---
+++ math.js
@@ -0,0 +1,10 @@
+//Exorsize math.js
+var numOne = 5;
+var numTwo = 10;
+var numThree = 15;
+var numFour = 20;
+var totalNumber;
+
+totalNumber = numOne / numTwo + numThree / numOne - numFour / numFour;
+
+console.log(totalNumber)
