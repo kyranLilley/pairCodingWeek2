7e885951838b92b2dc13f2243247a275fc287afb
kyran lilley <kyran-lilley@live.nmit.ac.nz>
2020-03-03 10:07:15 +1300

    big equation

--- math.js
+++ math.js
@@ -1,10 +1,11 @@
 //Exorsize math.js
 var numOne = 5;
 var numTwo = 10;
-var numThree = 15;
+var numThree = 1557;
 var numFour = 20;
 var totalNumber;
 
-totalNumber = (numOne / numTwo + numThree) / numOne// - numFour % numFour;
+//Equation of all these numbers below.
+totalNumber = ((numOne / numTwo + numThree) / numOne - numFour) % numFour;
 
-console.log( "5 / 10 + 15 / 5 - 20 remainder 20 = " + totalNumber)
+console.log( "5 / 10 + 1557 / 5 - 20 remainder 20 = " + totalNumber)
