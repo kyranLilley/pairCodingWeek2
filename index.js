e4705e3145309fb12e2056974318cdea33ccdb19
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-03 10:34:50 +1300

    Adding/refining comments

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-//Setting variables. 
+//Exorsise Index.js
+//Setting number variables. 
 var firstNumber = 15;
 var secondNumber = 20;
+//Calcualting results and adding to variables
 var minus = firstNumber - secondNumber;
 var plus = firstNumber + secondNumber;
 var divide = firstNumber / secondNumber;
 var remainder = secondNumber % firstNumber;
 var times = firstNumber * secondNumber;
 
-
+//Printing results individually
 //this line of code should 15 minus 20 should be -5.
 console.log( "15 - 20 = " + minus);
 //this line of code should  15 plus 20 should be = 35.
@@ -19,6 +21,5 @@ console.log( "20 % 15 , reminder = " + remainder);
 //this line will multiply first number(15) by second number(20) = 300.
 console.log( "15 * 20 = " + times);
 
-
-//all the equations in one line.
+//Printing results sequentially.
 console.log( "Concatination of all variable equations." + minus , plus , divide , remainder , times);
--- math.js
+++ math.js
@@ -1,11 +1,11 @@
-//Exorsize math.js
+//Exorcise math.js
+//Creating and setting variables
 var numOne = 5;
 var numTwo = 10;
 var numThree = 1557;
 var numFour = 20;
 var totalNumber;
-
-//Equation of all these numbers below.
+//Calculating result and adding to new variable
 totalNumber = ((numOne / numTwo + numThree) / numOne - numFour) % numFour;
-
+//Printing equation and result to console
 console.log( "5 / 10 + 1557 / 5 - 20 remainder 20 = " + totalNumber);
