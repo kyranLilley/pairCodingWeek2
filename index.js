915e76cfaaa395d2284b0cd2202f1ec255907170
kyran lilley <kyran-lilley@live.nmit.ac.nz>
2020-03-03 09:19:56 +1300

    Updating varibles

---
+++ index.js
@@ -0,0 +1,24 @@
+//Setting variables. 
+var firstNumber = 15;
+var secondNumber = 20;
+var minus = firstNumber - secondNumber;
+var plus = firstNumber + secondNumber;
+var divide = firstNumber / secondNumber;
+var remainder = firstNumber % secondNumber;
+var times = firstNumber * secondNumber;
+
+
+//this line of code should 15 minus 20 should be -5.
+console.log(minus);
+//this line of code should  15 plus 20 should be = 35.
+console.log(plus)
+//printing this equation to log , 15 divided by 20. 
+console.log(divide)
+//calculating the remainder
+console.log(remainder + "%")
+//this line will multiply first number(15) by second number(20) = 300.
+console.log(times)
+
+//general concat of both variables.
+console.log(firstNumber + secondNumber)
+
